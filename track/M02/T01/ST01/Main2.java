766e81c28eef7e3016ac16bb75b7c9df95faf15e
Pavitrareddy38 <pavitrareddy785@gmail.com>
2026-08-03 12:49:15 +0530

    Day2changes

---
+++ track/M02/T01/ST01/Main2.java
@@ -0,0 +1,19 @@
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        int javaHoursPerDay = 2;
+        int aptitudeHoursPerDay = 1;
+        int numberOfDays = 5;
+
+        int weeklyJavaHours = javaHoursPerDay * numberOfDays;
+
+        int weeklyAptitudeHours = aptitudeHoursPerDay * numberOfDays;
+
+        int totalPracticeHours = weeklyJavaHours + weeklyAptitudeHours;
+
+        System.out.println("Java: " + weeklyJavaHours);
+        System.out.println("Aptitude: " + weeklyAptitudeHours);
+        System.out.println("Total: " + totalPracticeHours);
+    }
+}
