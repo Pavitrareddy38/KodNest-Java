53fd15923714eae68f126539f2d4d41b4982d967
Pavitrareddy38 <pavitrareddy785@gmail.com>
2026-08-04 20:44:09 +0530

    day3changes

---
+++ track/practice/Pgm5.java
@@ -0,0 +1,25 @@
+
+public class Pgm5 {
+
+    public static void main(String[] args) {
+        int marks = 72;
+        int attendance = 80;
+        boolean backlog = false;
+        boolean project = true;
+
+        int mockInterviewScore = 65;
+        if (marks >= 60 && attendance >= 75 && !backlog) {
+            if (project) {
+                if (mockInterviewScore >= 70) {
+                    System.out.println("Placement Ready");
+                } else {
+                    System.out.println("Improve Interview Skills");
+                }
+            } else {
+                System.out.println("complete project");
+            }
+        } else {
+            System.out.println("Improve Acedamic Eligibility");
+        }
+    }
+}
