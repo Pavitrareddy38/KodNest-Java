7edbd6431d4264fd12f6b525c89829e4e4eb8705
Pavitrareddy38 <pavitrareddy785@gmail.com>
2026-08-04 21:29:15 +0530

    Day2changes

---
+++ track/M02/T01/ST01/P01/Main3.java
@@ -0,0 +1,10 @@
+
+public class Main3 {
+
+    public static void main(String[] args) {
+        String learnerName = "Salman";
+        int dailyPracticeHours = 8;
+        System.out.println("Name: " + learnerName);
+        System.out.println("Daily Practice Hours: " + dailyPracticeHours + " Hours");
+    }
+}
