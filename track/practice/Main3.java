dd4a851836f2ae679593c4e8b0b4fa9c90b70db3
Pavitrareddy38 <pavitrareddy785@gmail.com>
2026-08-05 16:09:50 +0530

    Day4Changes

---
+++ track/practice/Main3.java
@@ -0,0 +1,15 @@
+
+public class Main3 {
+
+    public static void main(String[] args) {
+
+        int num1 = 3;
+
+        int num2 = 8;
+
+        String res = (num1 > num2) ? num1 + " is greater than" + num2 : num2 + " is greater than" + num1;
+
+        System.out.println(res);
+
+    }
+}
