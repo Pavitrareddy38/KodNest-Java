7bf01fdac528d6df87beb3188ede1b527b118fe2
Pavitrareddy38 <pavitrareddy785@gmail.com>
2026-08-05 11:50:18 +0530

    Day4

---
+++ track/practice/Pgm6.java
@@ -0,0 +1,10 @@
+
+public class Pgm6 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int res = a++ + ++a + a++ + a++ + ++a + a++;
+        System.out.println(res);
+        System.out.println(a);
+    }
+}
