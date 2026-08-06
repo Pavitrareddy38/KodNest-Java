ad515fdb22a359f68be7ca83257fd0abad7528a0
Pavitrareddy38 <pavitrareddy785@gmail.com>
2026-08-06 12:50:14 +0530

    Day5changes

---
+++ track/M02/T02/ST04/P01/Break1.java
@@ -0,0 +1,14 @@
+
+public class Break1 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 2; j++) {
+                if (j == 3) {
+                    break;
+                }
+                System.out.println("i: " + i + " " + "j: " + j);
+            }
+        }
+    }
+}
