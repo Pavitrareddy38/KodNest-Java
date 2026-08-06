2d307cd0c47a4ed2242267c80bba943ea96a2cfe
Pavitrareddy38 <pavitrareddy785@gmail.com>
2026-08-06 13:01:27 +0530

    Day5changes

---
+++ track/M02/T02/ST04/Break2.java
@@ -0,0 +1,16 @@
+
+public class Break2 {
+
+    public static void main(String[] args) {
+        Outer:
+        for (int i = 1; i <= 3; i++) {
+            inner:
+            for (int j = 1; j <= 4; j++) {
+                if (i == 2) {
+                    break Outer;
+                }
+                System.out.println("i: " + i + " " + "j: " + j);
+            }
+        }
+    }
+}
