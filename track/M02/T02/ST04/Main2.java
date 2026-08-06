1982c116fede040f4499c545199f07d9244d89d2
Pavitrareddy38 <pavitrareddy785@gmail.com>
2026-08-06 19:38:01 +0530

    Day5changes

---
+++ track/M02/T02/ST04/Main2.java
@@ -0,0 +1,14 @@
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        int marks = 68;
+        int attendance = 80;
+        int practiceDays = 3;
+        String res = (marks >= 60 && attendance >= 75) ? "Placement Ready" : "Continue Preperation";
+        System.out.println(res);
+        for (int i = 1; i <= practiceDays; i++) {
+            System.out.println("Practice Day: " + i);
+        }
+    }
+}
