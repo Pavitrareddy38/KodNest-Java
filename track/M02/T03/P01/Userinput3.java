babf9c095cafba21d3afa8232fdad83015e0bc27
Pavitrareddy38 <pavitrareddy785@gmail.com>
2026-08-07 12:02:46 +0530

    Day6changes

---
+++ track/M02/T03/P01/Userinput3.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class Userinput2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter char: ");
+        char a = sc.next().charAt(2);
+        System.out.println(a);
+    }
+}
