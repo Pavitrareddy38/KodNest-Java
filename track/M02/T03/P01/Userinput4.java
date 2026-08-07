56590d9c3089b6515621b115cd27fd906e320532
Pavitrareddy38 <pavitrareddy785@gmail.com>
2026-08-07 12:19:52 +0530

    Day6changes

---
+++ track/M02/T03/P01/Userinput4.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Userinput4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String a = sc.next();
+        System.out.println(a);
+        System.out.println("Enter your solved problems: ");
+        int b = sc.nextInt();
+        System.out.println(b);
+        System.out.println("Enter your assessments completed");
+        double c = sc.nextDouble();
+        System.out.println(c);
+
+    }
+}
