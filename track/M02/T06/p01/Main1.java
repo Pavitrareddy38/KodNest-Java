4a975901eea102c05dd9565fa53b5fa57f35c80e
Pavitrareddy38 <pavitrareddy785@gmail.com>
2026-08-14 11:48:33 +0530

    Day10changes

---
+++ track/M02/T06/p01/Main1.java
@@ -0,0 +1,15 @@
+
+class Robot {
+
+    Robot() {
+        System.out.println("Beep beep! Robot reporting for Java duty");
+    }
+
+}
+
+class Main1 {
+
+    public static void main(String[] args) {
+        Robot r1 = new Robot();
+    }
+}
