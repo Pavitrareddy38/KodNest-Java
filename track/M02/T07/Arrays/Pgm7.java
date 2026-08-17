dd03bda141b9a05508605c7c1b22360adc5444e1
Pavitrareddy38 <pavitrareddy785@gmail.com>
2026-08-17 18:53:01 +0530

    Day12changes

---
+++ track/M02/T07/Arrays/Pgm7.java
@@ -0,0 +1,13 @@
+
+public class Pgm7 {
+
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30, 40, 50};
+        int bonus = 5;
+        System.out.print("Updated value: ");
+        for (int i = 0; i <= a.length - 1; i++) {
+            System.out.print((a[i] + bonus) + " ");
+        }
+
+    }
+}
