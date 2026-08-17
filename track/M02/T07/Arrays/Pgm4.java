3d3af5318806e472ade77cc728be18232da1c87a
Pavitrareddy38 <pavitrareddy785@gmail.com>
2026-08-17 12:47:49 +0530

    Day12changes

---
+++ track/M02/T07/Arrays/Pgm4.java
@@ -0,0 +1,15 @@
+
+public class Pgm4 {
+
+    public static void main(String[] args) {
+        int a[] = {11, 24, 35, 67, 99};
+        int correctionIndex = 2;
+        int correction = 3;
+        System.out.println("Reading count: " + a.length);
+        System.out.println("Before correction: " + a[correctionIndex]);
+        a[correctionIndex] += correction;
+        System.out.println("After correction: " + a[correctionIndex]);
+        System.out.println("Last index: " + (a.length-1));
+        
+    }
+}
