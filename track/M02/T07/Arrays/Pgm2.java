7fabb9cdb7df4c90cb2ce8d50e1c9ca72a711cdc
Pavitrareddy38 <pavitrareddy785@gmail.com>
2026-08-17 11:54:46 +0530

    Day12changes

---
+++ track/M02/T07/Arrays/Pgm2.java
@@ -0,0 +1,12 @@
+
+public class Pgm2 {
+
+    public static void main(String[] args) {
+        int a[] = {11, 24, 35, 67, 99};
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+    }
+}
