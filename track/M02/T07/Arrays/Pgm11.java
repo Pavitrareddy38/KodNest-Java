e56aee64ef5f86cd5a8955328bf7833739012949
Pavitrareddy38 <pavitrareddy785@gmail.com>
2026-08-18 12:35:12 +0530

    Day13changes

---
+++ track/M02/T07/Arrays/Pgm11.java
@@ -0,0 +1,12 @@
+
+public class Pgm11 {
+
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 10, 30, 10, 30, 10, 40};
+        int key = 10;
+        Freq f = new Freq();
+        f.count(arr, key);
+
+    }
+
+}
