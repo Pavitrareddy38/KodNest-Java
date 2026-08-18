0af3b1495c5b38ea3dd15ef15ea052e4efb5cfcc
Pavitrareddy38 <pavitrareddy785@gmail.com>
2026-08-18 11:56:19 +0530

    Day13changes

---
+++ track/M02/T07/Arrays/ArrayFound.java
@@ -0,0 +1,18 @@
+
+public class ArrayFound {
+
+    void search(int[] ref, int key) {
+        boolean found = false;
+        for (int i = 0; i <= ref.length - 1; i++) {
+            if (ref[i] == key) {
+                System.out.println("Element found at index: " + i);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("System not found..");
+        }
+
+    }
+}
---
+++ track/M02/T07/Arrays/ArraySearch.java
@@ -0,0 +1,12 @@
+
+public class ArraySearch {
+
+    public static void main(String[] args) {
+        int a[] = {12, 34, 56, 78, 99};
+        int key = 99;
+        ArrayFound af = new ArrayFound();
+        af.search(a, key);
+
+    }
+
+}
