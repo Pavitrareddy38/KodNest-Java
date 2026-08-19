cf9d95728f2caabe36326dc4197b90ad130f40c4
Pavitrareddy38 <pavitrareddy785@gmail.com>
2026-08-19 12:16:41 +0530

    Day14changes

--- track/M02/T08/Pgm1.java
+++ track/M02/T08/Pgm1.java
@@ -4,17 +4,21 @@ import java.util.Scanner;
 public class Pgm1 {
 
     public static void main(String[] args) {
-        int a[][] = new int[3][5];
         Scanner s = new Scanner(System.in);
+        System.out.println("Enter the size of array elements: ");
+        int r = s.nextInt();
+        int c = s.nextInt();
+        int a[][] = new int[r][c];
+
         System.out.println("Enter the array elements: ");
-        for (int i = 0; i <= 2; i++) {
-            for (int j = 0; j <= 4; j++) {
+        for (int i = 0; i <= r - 1; i++) {
+            for (int j = 0; j <= c - 1; j++) {
                 a[i][j] = s.nextInt();
             }
         }
         System.out.println("Array elements are: ");
-        for (int i = 0; i <= 2; i++) {
-            for (int j = 0; j <= 4; j++) {
+        for (int i = 0; i <= r - 1; i++) {
+            for (int j = 0; j <= c - 1; j++) {
                 System.out.print(a[i][j] + " ");
             }
             System.out.println();
