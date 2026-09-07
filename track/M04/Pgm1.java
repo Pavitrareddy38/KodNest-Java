8bdc0440915fb0648d1a5b2ad403bbfda40600fa
Pavitrareddy38 <pavitrareddy785@gmail.com>
2026-09-07 17:30:20 +0530

    Day18changes

---
+++ track/M04/Pgm1.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Pgm1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String learnerName = scanner.nextLine();
+        String city = scanner.nextLine();
+
+        // Display the name, city and name length.
+        System.out.println("Name: " + learnerName);
+        System.out.println("City: " + city);
+        System.out.println("Name length: " + learnerName.length());
+        scanner.close();
+    }
+}
