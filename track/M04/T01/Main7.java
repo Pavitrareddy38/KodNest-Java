9f15e2b81718c9e1e50073e53744132d0f32a34e
Pavitrareddy38 <pavitrareddy785@gmail.com>
2026-09-08 17:42:32 +0530

    Day18changes

---
+++ track/M04/T01/Main7.java
@@ -0,0 +1,12 @@
+
+public class Main7 {
+
+    public static void main(String[] args) {
+        String s = " java ";
+        System.out.println(s);
+        System.out.println(s.length());
+        System.out.println(s.trim());
+        System.out.println(s.trim().length());
+
+    }
+}
